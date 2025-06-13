6e71ef5926922660a5c67eb9affe73512bef34fc
AMK1215 <aungmyokhaing1215@gmail.com>
2025-06-13 10:51:31 +0630

    client

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            PaymentTypeTableSeeder::class,
             PermissionsTableSeeder::class,
             RolesTableSeeder::class,
             PermissionRoleTableSeeder::class,
